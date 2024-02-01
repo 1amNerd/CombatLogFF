c7e407c062ddb4d1e356b44a79e1c6e6d4998dda
1amNerd <131608602+1amNerd@users.noreply.github.com>
2024-02-01 13:01:52 -0500

    Add files via upload

---
+++ forge/src/main/java/me/toastymop/combatlog/forge/CombatLogForge.java
@@ -0,0 +1,12 @@
+package me.toastymop.combatlog.forge;
+
+import me.toastymop.combatlog.CombatConfig;
+import me.toastymop.combatlog.CombatLog;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(CombatLog.MOD_ID)
+public class CombatLogForge {
+    public CombatLogForge() {
+        CombatConfig.getConfig();
+    }
+}
